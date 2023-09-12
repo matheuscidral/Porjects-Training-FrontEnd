68c8174b43a05fc9961b4e5c152b5a2251d285dd
MatheusCidral <matheus.costa.cidral@gmail.com>
2023-09-12 20:02:58 -0300

    Finalização de progress-steps

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,11 +1,11 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.getElementById('.circle');
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
 let currentActive = 1;
 
-next.addEventListener('click', () => {
+next.addEventListener("click", () => {
   currentActive++;
 
   if (currentActive > circles.length) {
@@ -15,7 +15,7 @@ next.addEventListener('click', () => {
   update();
 });
 
-prev.addEventListener('click', () => {
+prev.addEventListener("click", () => {
   currentActive--;
 
   if (currentActive < 1) {
@@ -28,15 +28,16 @@ prev.addEventListener('click', () => {
 function update() {
   circles.forEach((circle, idx) => {
     if (idx < currentActive) {
-      circle.classList.add('active');
+      circle.classList.add("active");
     } else {
-      circle.classList.remove('active');
+      circle.classList.remove("active");
     }
   });
 
-  const actives = document.querySelectorAll('.active');
+  const actives = document.querySelectorAll(".active");
 
-  progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
 
   if (currentActive === 1) {
     prev.disabled = true;
